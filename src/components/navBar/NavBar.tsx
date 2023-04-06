f4570dfd64a66388fdec441eaf974dde38d7f52d
Andrey <spacecmetanka@gmail.com>
2023-04-06 16:01:15 +0300

    refactor: nav bar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './navBar.scss';
 
-export default function NavBar() {
+import styles from './navBar.module.scss';
+
+export function NavBar() {
   return (
-    <nav className="nav">
+    <nav className={styles.nav}>
       <Link to={'/'}>HOME</Link>
       <Link to={'/about'}>ABOUT</Link>
       <Link to={'/form'}> FORM </Link>
