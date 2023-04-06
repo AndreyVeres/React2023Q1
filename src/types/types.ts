af87f8969c9a830a145c124d315d90d771122e86
Andrey <spacecmetanka@gmail.com>
2023-04-06 16:00:42 +0300

    feat: add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,27 @@
-export interface IProducts {
-  brand: string;
-  category: string;
-  description: string;
-  discountPercentage: number;
-  id: number;
-  images: string[];
-  price: number;
-  rating: number;
-  stock: number;
-  thumbnail: string;
-  title: string;
-}
-
-export interface IFormValues {
-  [key: string]: string | undefined | null | boolean;
+export interface ICard {
+  artist: string;
+  cmc: number;
+  colorIdentity: string[];
+  colors: string[];
+  id: string;
+  imageUrl: string;
+  layout: string;
+  manaCost: string;
+  multiverseid: string;
+  name: string;
+  number: string;
+  originalText: string;
+  originalType: string;
+  power: string;
+  prinntings: string[];
+  rarity: string;
+  set: string;
+  setName: string;
+  subtypes: string[];
+  text: string;
+  toughness: string;
+  type: string;
+  types: string[];
+  variations: string[];
+  flavor: string;
 }
